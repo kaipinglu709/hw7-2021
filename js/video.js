b050e7bcb72f62427af7f6adf399e2589fec958b
Kaiping Lu <kaipingl@umich.edu>
2024-04-02 21:36:00 -0400

    1st commit

--- js/video.js
+++ js/video.js
@@ -2,6 +2,8 @@ var video;
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
+	video = document.querySelector("#player1"); 
+	video.loop = false; 
 
 });
 
@@ -9,3 +11,60 @@ window.addEventListener("load", function() {
 // 	console.log("Play Video");
 // });
 
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML = Math.round(video.volume * 100) + "%";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+// Slow
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate *= 0.9;
+	console.log("New speed: " + video.playbackRate);
+});
+
+// Speed Up
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate /= 0.9;
+	console.log("New speed: " + video.playbackRate);
+});
+
+// Skip Ahead
+document.querySelector("#skip").addEventListener("click", function() {
+	if(video.currentTime + 10 > video.duration) {
+		video.currentTime = 0;
+	} else {
+		video.currentTime += 10;
+	}
+	console.log("Current time: " + video.currentTime);
+});
+
+// Mute
+document.querySelector("#mute").addEventListener("click", function() {
+	video.muted = !video.muted;
+	this.innerHTML = video.muted ? "Unmute" : "Mute";
+	console.log("Mute status:" + video.muted);
+});
+
+// Volume Slider
+document.querySelector("#slider").addEventListener("change", function() {
+	video.volume = this.value / 100;
+	document.querySelector("#volume").innerHTML = this.value + "%";
+	console.log("New Volume " + video.volume);
+});
+
+// Styled Old school
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+	console.log("Old School style applied");
+});
+//original
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+	console.log("Original style restored");
+});
